2d4c27e87b2fa32496695a9d25c3e58e6c17df1b
Garin <garin.kra@gmail.com>
2020-12-18 14:14:20 +0700

    fix: change the algorith for soal 1
    sebelumnya hanya menambahkan tulisan watermark ke foto.
    seharusnya menambahkan gambar watermark ke foto

--- soal1.py
+++ soal1.py
@@ -3,35 +3,51 @@ from PIL import Image, ImageDraw, ImageFont
 from datetime import datetime
  
 # create Image object with the input image
-image = Image.open('background.png')
-W, H =  image.size
+foto = Image.open('foto.png')
+W, H =  foto.size
 
-original = Image.open('background.png')
  
 # initialise the drawing context with
-# the image object as background
-draw = ImageDraw.Draw(image)
+# the foto object as background
+draw = ImageDraw.Draw(foto)
 
 # create font object with the font file and specify
 # desired size
 font = ImageFont.truetype('Montserrat-Regular.ttf', size=30)
+#spicify the color
+color = 'rgb(255, 255, 255)' # white color
+
  
-# starting position of the text
+# create the text for watermark
 identitas = "18218050 - Garin Ichsan Nugraha"
 w1, h1 = draw.textsize(identitas, font=font)
-(x, y) = ((W-w1)/2, H - (3*h1))
-color = 'rgb(255, 255, 255)' # black color
-
-# draw the text on the background
-draw.text((x, y), identitas, fill=color, font=font)
 
 timestamp = str(datetime.now())
 w2, h2 = draw.textsize(timestamp, font=font)
-(x, y) = ((W-w2)/2, H - (2*h1))
-color = 'rgb(255, 255, 255)' # black color
-draw.text((x, y), timestamp, fill=color, font=font)
- 
-# save the edited image
-image.save('soal1.png')
 
-original.save('soal1.gif', save_all=True, append_images=[image], optimize=False, duration=500, loop=0)
+#create rectangle image for the watermark backgroung image
+Hrec = round(h1+h2+(h1+h2)/2) #cuztomize the rectangle hight first
+rectangle = Image.new('RGB', (W, (Hrec)), "black")
+
+watermark = ImageDraw.Draw(rectangle)
+
+# spicify the position for identitas text
+(x, y) = ( round((W-w1)/2), round((0.5*h1)))
+# draw the text on the rectangle image
+watermark.text((x, y), identitas, fill=color, font=font)
+
+# spicify the position for identitas text
+(x, y) = ( round((W-w2)/2), round((1.5*h1)))
+# draw the text on the rectangle image
+watermark.text((x, y), timestamp, fill=color, font=font)
+
+foto.paste(rectangle,(0,H-Hrec))
+
+# save the watermark image
+foto.save('watermarked.png')
+
+#get the original file again to create the gif
+original = Image.open('foto.png')
+
+#create and save the gif
+original.save('soal1.gif', save_all=True, append_images=[foto], optimize=False, duration=500, loop=0)
